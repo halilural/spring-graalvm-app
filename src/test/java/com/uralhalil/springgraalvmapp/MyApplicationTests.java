15dc669c0766dc91e70a93de61ac0183d56cee82
halilural <halilural5@gmail.com>
2024-11-26 00:15:46 +0100

    - create the spring boot app - add graalvm buildtools support - add spring boot starter web dependency

---
+++ src/test/java/com/uralhalil/springgraalvmapp/MyApplicationTests.java
@@ -0,0 +1,13 @@
+package com.uralhalil.springgraalvmapp;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringGraalvmAppApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
